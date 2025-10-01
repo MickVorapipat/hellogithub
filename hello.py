8a6d3d79188ee30540655acdd8540191d10ab698
MickVorapipat <mick.vorapipat@hotmail.com>
2025-10-01 20:00:48 +0700

    Move hello world to function

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
-print('hello')
-print('hello mick')
-print('hello mick vorapipat')
+# this is a hello world file
+def helloworld():
+    print('hello')
+    print('hello mick')
+    print('hello mick vorapipat')
+
+helloworld()
